a86cce0d24878eb5060b05a63714caa0dc747cb3
kylem148 <jazzerflash@gmail.com>
2025-10-17 14:01:14 -0700

    feat: navbar basic styling

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -7,13 +7,35 @@ export default function Navbar() {
     // with your navbar code from your earlier milestones
     // NOTE: we use "class" in HTML but React is quirky so we have to
     // change all the "class" to "className"
-    <header className="bg-neutral-600 md:px-20 px-10 md:py-5 py-3 flex justify-between items-center">
-      <h1> Kyle Morgan </h1>
-      <nav className="flex gap-5 ">
-        <Link href="/">Home</Link>
-        <Link href="/blogs">Blogs</Link>
-        <Link href="/resume">Resume</Link>
-        <Link href="/about">Contact Me</Link>
+    <header className="bg-neutral-600 md:px-13 px-10 py-3 flex justify-between items-center">
+      <div className="md:text-[30px] text-[20px]">
+        <h1> Kyle Morgan </h1>
+      </div>
+      <nav className="flex gap-5 text-green-200 md:text-[22px] text-[17px]">
+        <Link
+          href="/"
+          className="transition-transform duration-300 ease-in-out hover:-translate-y-1"
+        >
+          Home
+        </Link>
+        <Link
+          href="/blogs"
+          className="transition-transform duration-300 ease-in-out hover:-translate-y-1"
+        >
+          Blogs
+        </Link>
+        <Link
+          href="/resume"
+          className="transition-transform duration-300 ease-in-out hover:-translate-y-1"
+        >
+          Resume
+        </Link>
+        <Link
+          href="/about"
+          className="transition-transform duration-300 ease-in-out hover:-translate-y-1"
+        >
+          Contact Me
+        </Link>
       </nav>
     </header>
   );
