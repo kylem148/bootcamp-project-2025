2c13deab23d75de81215e2384588407b96bce7ff
kylem148 <jazzerflash@gmail.com>
2025-10-17 20:22:53 -0700

    feat: setup index.js, and css file for marquee

---
+++ bootcamp-milestone-2/src/constants/index.js
@@ -0,0 +1,44 @@
+const iconsList = [
+  {
+    name: "html",
+    image: "/html.svg",
+  },
+  {
+    name: "css",
+    image: "/css.svg",
+  },
+  {
+    name: "javascript",
+    image: "/js.svg",
+  },
+  {
+    name: "react",
+    image: "/react.svg",
+  },
+  {
+    name: "typescript",
+    image: "/ts.svg",
+  },
+  {
+    name: "github",
+    image: "/github.svg",
+  },
+  {
+    name: "gsap",
+    image: "/gsap.svg",
+  },
+  {
+    name: "threejs",
+    image: "/gsap.svg",
+  },
+  {
+    name: "figma",
+    image: "/gsap.svg",
+  },
+  {
+    name: "aws",
+    image: "/gsap.svg",
+  },
+];
+
+export { iconsList };
