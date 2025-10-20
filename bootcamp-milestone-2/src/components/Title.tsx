ab9e7b6d5140dc13b29e6f69c2d7147a66215019
kylem148 <jazzerflash@gmail.com>
2025-10-20 14:19:06 -0700

    feat: add gradient

--- bootcamp-milestone-2/src/components/Title.tsx
+++ bootcamp-milestone-2/src/components/Title.tsx
@@ -1,11 +1,25 @@
 function Title() {
   return (
     <>
-      <div className="z-2000 transition-transform duration-100 ease-out w-screen h-screen bg-neutral-900">
-        <h1 className="flex items-center justify-center h-screen text-white text-6xl font-bold">
-          Kyle Morgan
-        </h1>
-      </div>
+      <main>
+        {/* Gradient image */}
+
+        <img
+          className="absolute top-0 right-0 opacity-60 -z-10"
+          src="/gradient.png"
+          alt="Gradient-img"
+        />
+
+        {/* Blur Effect */}
+
+        <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg]"></div>
+
+        <div className="z-2000 transition-transform duration-100 ease-out w-screen h-screen bg-neutral-900">
+          <h1 className="flex items-center justify-center h-screen text-white text-6xl font-bold">
+            Kyle Morgan
+          </h1>
+        </div>
+      </main>
     </>
   );
 }
