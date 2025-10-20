50e34a879742bf65ba8efa9775ad306249862adc
kylem148 <jazzerflash@gmail.com>
2025-10-20 14:25:44 -0700

    feat: update navbar

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -5,8 +5,6 @@ import { useGSAP } from "@gsap/react";
 import "boxicons/css/boxicons.min.css";
 
 function Navbar() {
-  const [titleDone, setTitleDone] = useState(false);
-
   //toggle visiblity of menu
   const toggleMenu = () => {
     const MobileMenu = document.getElementById("mobileMenu");
@@ -17,107 +15,79 @@ function Navbar() {
     }
   };
 
-  const showNavbar = () => {
-    const tl = gsap.timeline();
-    tl.fromTo(
-      "header",
-      { y: -100, opacity: 0 },
-      { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
-    );
-    tl.fromTo(
-      "header h1",
-      { x: -50, opacity: 0 },
-      {
-        x: 0,
-        opacity: 1,
-        duration: 0.5,
-        ease: "power2.out",
-        onComplete: () => setTitleDone(true),
-      },
-      "-=0.5"
-    );
-    tl.fromTo(
-      "nav a",
-      { y: -20, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 0.3,
-        ease: "power2.out",
-        stagger: 0.1,
-      },
-      "-=0.3"
-    );
-  };
-
-  useGSAP(() => {
-    showNavbar();
-  }, []);
-
   return (
     // Navbar component for desktop
-    <header className="z-90 flex justify-between items-center py-4 px-4 lg:px-20 fixed right-0 left-0 top-0 bg-nuetral-900 bg-opacity-70 backdrop-blur-md shadow-md">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl m-0">Kyle Morgan</h1>
+    <header
+      id="desktopMenu"
+      className="z-90 flex justify-between items-center py-4 px-4 lg:px-20 fixed right-0 left-0 top-0 bg-nuetral-900 bg-opacity-70 backdrop-blur-md shadow-md"
+    >
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold m-0">
+        KYLE MORGAN
+      </h1>
       <nav className="hidden md:flex items-center gap-12">
         <a
           className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
           href="#"
         >
-          Home
+          HOME
         </a>
         <a
           className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
           href="#"
         >
-          Blog
+          BLOG
         </a>
         <a
           className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
           href="#"
         >
-          Resume
-        </a>
-        <a
-          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
-          href="#"
-        >
-          Contact
+          RESUME
         </a>
       </nav>
 
-      <button onClick={toggleMenu} className="md:hidden text-3xl p-2 z-50">
+      <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-white cursor-pointer z-50">
+        CONTACT
+      </button>
+
+      {/* Mobile Button */}
+      <button
+        onClick={() => {
+          toggleMenu();
+        }}
+        className="md:hidden text-3xl p-2 z-50"
+      >
         <i className="bx bx-menu"></i>
       </button>
 
       {/* Mobile Menu */}
       <div
         id="mobileMenu"
-        className="hidden fixed top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg0black bg-opacity-70 backdrop-blur- md"
+        className="hidden fixed top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40"
       >
         <nav className="flex flex-col gap-6 items-center">
           <a
             className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
             href="#"
           >
-            Home
+            HOME
           </a>
           <a
             className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
             href="#"
           >
-            Blog
+            BLOG
           </a>
           <a
             className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
             href="#"
           >
-            Resume
+            RESUME
           </a>
           <a
             className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
             href="#"
           >
-            Contact
+            CONTACT
           </a>
         </nav>
       </div>
