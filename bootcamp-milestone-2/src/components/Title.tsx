57c24d92f809c10306ca5da57138430f0b43a8e4
kylem148 <jazzerflash@gmail.com>
2025-10-20 00:35:13 -0700

    refactor: changes font size

--- bootcamp-milestone-2/src/components/Title.tsx
+++ bootcamp-milestone-2/src/components/Title.tsx
@@ -1,7 +1,7 @@
 function Title() {
   return (
     <>
-      <div className="transition-transform duration-100 ease-out w-screen h-screen bg-neutral-900">
+      <div className="z-2000 transition-transform duration-100 ease-out w-screen h-screen bg-neutral-900">
         <h1 className="flex items-center justify-center h-screen text-white text-6xl font-bold">
           Kyle Morgan
         </h1>
--- bootcamp-milestone-2/src/sections/AboutMe.tsx
+++ bootcamp-milestone-2/src/sections/AboutMe.tsx
@@ -5,7 +5,7 @@ function AboutMe() {
     <div className="md:flex flex-col h-screen w-screen overflow-hidden p-20 py-30">
       <div className="flex-1 flex flex-row">
         <div className="flex flex-col flex-shrink basis-2/3 justify-center items-left flex-wrap">
-          <h1 className="text-5xl font-bold pl-5 mb-5">About me</h1>
+          <h1 className="text-5xl md:text-6xl font-bold pl-5 mb-5">About me</h1>
           <p className="text-[20px] p-5 text-left">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
             qui sunt vitae aspernatur reiciendis hic odio labore placeat dolor,
