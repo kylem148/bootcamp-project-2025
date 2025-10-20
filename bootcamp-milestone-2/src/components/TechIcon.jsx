56f005dbaa75b749f1f34cad27ea79c06ac41410
kylem148 <jazzerflash@gmail.com>
2025-10-20 00:34:34 -0700

    fix: marquee tech icon spacing

--- bootcamp-milestone-2/src/components/TechIcon.jsx
+++ bootcamp-milestone-2/src/components/TechIcon.jsx
@@ -1,6 +1,6 @@
 const TechIcon = ({ icon }) => {
   return (
-    <div className="md:w-32 md:h-32 w-20 h-20 flex-center hover:-translate-y-3 transition-all duration-700">
+    <div className="flex-center hover:-translate-y-3 transition-all duration-700">
       <img src={icon.image} alt={icon.name} className="md:size-16 size-10" />
     </div>
   );
