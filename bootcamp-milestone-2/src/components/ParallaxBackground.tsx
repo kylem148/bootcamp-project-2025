f7c5fa166b96d7c131bf829ff9b68fb07a8a744b
kylem148 <jazzerflash@gmail.com>
2025-10-22 18:32:36 -0700

    feat: update parallax to feature new image

--- bootcamp-milestone-2/src/components/ParallaxBackground.tsx
+++ bootcamp-milestone-2/src/components/ParallaxBackground.tsx
@@ -10,34 +10,30 @@ import {
 } from "framer-motion";
 
 type Props = {
-  targetRef: RefObject<HTMLElement>;
+  targetRef: RefObject<HTMLElement | null>;
 };
 
 const ParallaxBackground = ({ targetRef }: Props) => {
   const { scrollYProgress } = useScroll({
     target: targetRef,
-    offset: ["start 0%", "end 20%"],
+    offset: ["start 0%", "end 40%"],
   });
 
   useMotionValueEvent(scrollYProgress, "change", (v) => {});
 
-  const eased = useSpring(scrollYProgress, { damping: 40, stiffness: 120 });
+  const eased = useSpring(scrollYProgress, { damping: 30, stiffness: 120 });
 
   const mountain3Y = useTransform(eased, [0, 1], ["0%", "70%"]);
   const planetsX = useTransform(eased, [0, 1], ["0%", "-20%"]);
-  const title = useTransform(eased, [0,1], ["0%", "-20%"]);
   const mountain2Y = useTransform(eased, [0, 1], ["0%", "30%"]);
   const mountain1Y = useTransform(eased, [0, 1], ["0%", "0%"]);
 
   return (
     <div className="absolute inset-0 pointer-events-none">
       <div className="relative h-full flex items-center justify-center">
-        <div
-          className="inset-0 -z-50 text-6xl md:text-8xl font-bold opacity-90"
-        >
-          CONTACT ME  
-        </div> 
-        
+        <div className="inset-0 -z-50 text-6xl md:text-8xl font-bold opacity-90">
+          CONTACT ME
+        </div>
 
         {/* Mountain Layer 3 */}
         <motion.div
@@ -52,7 +48,7 @@ const ParallaxBackground = ({ targetRef }: Props) => {
 
         {/* Planets */}
         <motion.div
-          className="absolute inset-0 -z-30"
+          className="absolute inset-0 -z-30 md:-mb-60 overflow-visible"
           style={{
             backgroundImage: "url(/planets.png)",
             backgroundPosition: "bottom",
