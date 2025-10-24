5e89784048917557282634d22174be69cda73f7e
kylem148 <jazzerflash@gmail.com>
2025-10-24 14:13:33 -0700

    feat: adding text graident

--- bootcamp-milestone-2/src/sections/AboutMe.tsx
+++ bootcamp-milestone-2/src/sections/AboutMe.tsx
@@ -5,7 +5,7 @@ function AboutMe() {
     <div id="about"className=" md:flex flex-col h-screen w-screen overflow-hidden p-5 md:p-20 py-30 scroll-mt-20">
       <div className="flex-1 flex flex-col md:flex-row">
         <div className="flex flex-col flex-shrink basis-2/3 justify-center items-center md:items-left flex-wrap">
-          <h1 className="text-5xl md:text-6xl font-bold pl-5 mb-5">About me</h1>
+          <h1 className="text-5xl md:text-6xl font-bold bg-gradient-to-r from-white to-green-400 text-transparent bg-clip-text pl-5 mb-5">About me</h1>
           <p className="text-[20px] p-5 text-left">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
             qui sunt vitae aspernatur reiciendis hic odio labore placeat dolor,
