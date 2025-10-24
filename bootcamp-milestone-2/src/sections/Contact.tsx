98dd6f7804b7099f56ec008e1cec2d78ab01b2d3
kylem148 <jazzerflash@gmail.com>
2025-10-24 14:13:47 -0700

    refactor: update color

--- bootcamp-milestone-2/src/sections/Contact.tsx
+++ bootcamp-milestone-2/src/sections/Contact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id="contact" className="grid mt-20 md:mt-50 lg:grid-cols-2 items-start gap-16 p-6 mx-auto max-w-5xl max-lg:max-w-2xl bg-neutral-700 border-6 border-gray-600 rounded-2xl scroll-mt-20">
+    <div id="contact" className="grid mt-20 md:mt-50 lg:grid-cols-2 items-start gap-16 p-6 mx-auto max-w-5xl max-lg:max-w-2xl bg-neutral-700 border-6 border-neutral-800 rounded-2xl scroll-mt-20">
       <div>
         <h2 className="text-white text-3xl font-bold">Let's Talk</h2>
         <p className="text-[15px] text-neutral-200 mt-4 leading-relaxed">Have some big idea or brand to develop and need help? Then reach out I'd love to hear about your project  and provide help.</p>
@@ -73,7 +73,7 @@ const Contact = () => {
         <textarea placeholder='Message'
           className="w-full rounded-md px-4 bg-slate-100 text-slate-900 text-sm pt-3 border border-gray-200 focus:border-slate-900 outline-none "></textarea>
         <button type='button'
-          className="text-white bg-slate-900 hover:bg-slate-800 tracking-wide rounded-md text-sm font-medium px-4 py-3 w-full cursor-pointer !mt-2 border-0">Send message</button>
+          className="text-white bg-neutral-900 hover:bg-neutral-800 tracking-wide rounded-md text-sm font-medium px-4 py-3 w-full cursor-pointer !mt-2 border-0">Send message</button>
       </form>
     </div>
   )
