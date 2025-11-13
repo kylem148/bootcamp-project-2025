a8561890b04de6a5d306509c8d3d4fc2517bbf23
kylem148 <jazzerflash@gmail.com>
2025-11-12 17:38:47 -0800

    feat: nest comment schema inside blogs

--- bootcamp-milestone-2/src/database/blogSchema.ts
+++ bootcamp-milestone-2/src/database/blogSchema.ts
@@ -1,5 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
+// Comment type for nested comments
+type IComment = {
+  name: string;
+  timeAgo: Date;
+  text: string;
+};
+
 // typescript type (can also be an interface)
 type Blog = {
   title: string;
@@ -8,7 +15,15 @@ type Blog = {
   image: string;
   imageAlt: string;
   slug: string;
+  comments: IComment[];
 };
+
+// Comment schema for nesting
+const commentSchema = new Schema<IComment>({
+  name: { type: String, required: true },
+  timeAgo: { type: Date, default: Date.now },
+  text: { type: String, required: true },
+});
 
 // mongoose schema
 const blogSchema = new Schema<Blog>({
@@ -18,9 +33,11 @@ const blogSchema = new Schema<Blog>({
   image: { type: String, required: true },
   imageAlt: { type: String, required: true },
   slug: { type: String, required: true, unique: true },
+  comments: [commentSchema], // Array of nested comments
 });
 
 // defining the collection and model
 const Blog = mongoose.models["blogs"] || mongoose.model("blogs", blogSchema);
 
 export default Blog;
+export type { Blog as IBlog, IComment };
