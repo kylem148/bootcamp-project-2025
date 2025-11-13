a201a54436249fa0e1a90cce9d5234df9bd40504
kylem148 <jazzerflash@gmail.com>
2025-11-12 16:02:50 -0800

    refactor: small comments update

--- bootcamp-milestone-2/src/app/blogs/[slug]/page.tsx
+++ bootcamp-milestone-2/src/app/blogs/[slug]/page.tsx
@@ -53,9 +53,7 @@ export default async function BlogPage({
 
     return (
       <main>
-        {/* Top section */}
         <section className="relative w-screen h-[50vh] flex flex-col justify-center overflow-hidden p-10 md:p-30">
-          {/* Background image */}
           <Image
             src={doc.image}
             alt={doc.imageAlt}
@@ -66,10 +64,8 @@ export default async function BlogPage({
             unoptimized
           />
 
-          {/* Overlay */}
           <div className="absolute inset-0 bg-black/40" />
 
-          {/* Foreground text */}
           <div className="relative z-10">
             <h1 className="font-bold text-amber-50 opacity-90 text-5xl md:text-7xl">
               {doc.title}
@@ -80,7 +76,6 @@ export default async function BlogPage({
           </div>
         </section>
 
-        {/* Bottom section */}
         <section className="p-10 flex flex-col lg:flex-row gap-10 max-w-7xl mx-auto">
           <div className="lg:w-1/2 p-5">
             <p className="text-lg md:text-xl text-gray-700 dark:text-gray-300 leading-relaxed">
@@ -90,7 +85,7 @@ export default async function BlogPage({
               href="/#blog"
               className="mt-10 inline-block text-[0.95rem] text-green-500 hover:text-green-400 transition-colors duration-200 font-medium"
             >
-              ← Go back to all blogs
+              ...Go back to all blogs
             </a>
           </div>
           <CommentsSection />
