bd85681e7576c16009a36cb7124bd617c9533199
kylem148 <jazzerflash@gmail.com>
2025-11-19 17:35:05 -0800

    feat: scroll arrow on title screen

--- bootcamp-milestone-2/src/components/Title.tsx
+++ bootcamp-milestone-2/src/components/Title.tsx
@@ -1,8 +1,34 @@
 import { useGSAP } from "@gsap/react";
+import { GoArrowDown } from "react-icons/go";
 import gsap from "gsap";
 
 function Title() {
+  useGSAP (() => {
+    
+    gsap.fromTo('#scroll',{      
+      x: 100,
+      opacity: 0,
+      
+    },{
+      x: 0,
+      opacity: 50,
+      duration: 1,
+      delay: 2,
+    })
+    
+    
+    gsap.to('#scroll',{
+      duration: 0.8, 
+      y: -10,
+      repeat: -1,
+      yoyo: true,
+      delay: 3,
 
+    })
+
+  }, [])
+  
+  
   return (
     <>
       <main>
@@ -18,10 +44,18 @@ function Title() {
 
         <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#60ad2d] -rotate-[30deg]"></div>
 
-        <div className="z-2000 transition-transform duration-100 ease-out w-screen h-screen bg-neutral-900">
-          <h1 className="title-text text-white flex items-center justify-center h-screen text-6xl font-bold">
+        <div className="z-2000 flex flex-col transition-transform duration-100 ease-out w-screen h-screen bg-neutral-900">
+          <h1 className="title-text flex w-full h-full text-white items-center justify-center text-6xl font-bold">
             Kyle Morgan
           </h1>
+          <div id="scroll" className="text-white flex items-center justify-end h-4 pb-10 pr-15 ">
+            <div className="text-xl">
+              scroll
+            </div>
+            <div id="d_arrow" className="text-2xl">
+              <GoArrowDown />
+            </div>
+          </div>
         </div>
       </main>
     </>
